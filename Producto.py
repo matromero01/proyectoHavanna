8f6b72dba08625e22b0810f310a4ee3c5d372956
matiasrom01 <matiastomromero@gmail.com>
2026-03-23 19:49:06 -0300

    Menu principal y producto

---
+++ Producto.py
@@ -0,0 +1,37 @@
+
+def productoMenu():
+    while opcion != 0:
+        print('Menu Producto\n' 
+        '1 - Alta Producto\n' 
+        '2 - Baja Producto\n' 
+        '3 - Modificacion Producto ...\n'
+        '4 - Mostrar Productos ...\n'
+        '0 - Volver al menu principal\n')
+
+        opcion = int(input("Ingresa un numero: "))
+        if opcion == 1:
+            altaProducto()
+        if opcion == 2:
+            bajaProducto()
+        if opcion == 3:
+            modificacionProducto()
+        if opcion == 4:
+            mostrarListaProducto()
+        if opcion == 0:
+            print("Adios!") 
+        
+        
+    
+def altaProducto():
+    productoNombre = input("Ingrese el nombre del producto: ")
+    productoPrecio = input("Ingrese el precio del producto: ")
+    productoCantidad = input("Ingrese el stock del producto: ")
+
+def bajaProducto():
+    idProducto = input("Ingrese el ID del producto: ")
+
+def modificacionProducto():
+    idProducto = input("Ingrese el ID del producto: ")
+
+def mostrarListaProducto():
+    print("Listado")
--- inicio.py
+++ inicio.py
@@ -1,1 +1,18 @@
-print("Inicio de Havanna")
+import Producto
+import os
+
+#print("Inicio de Havanna")
+
+opcion = 1
+while opcion != 0:
+    print('Menu principal\n' 
+    '1 - Menu Producto\n' 
+    '2 - Menu ...\n' 
+    '3 - Menu ...\n'
+    '0 - Salir\n')
+
+    opcion = int(input("Ingresa un numero: "))
+    if opcion == 1:
+        Producto.productoMenu()
+    if opcion == 0:
+        print("Adios!")     
